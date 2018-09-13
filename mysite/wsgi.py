417b77c6425cc404489b25e66b2eca65f9436de3
demiurgo64 <alanpalacio64@hotmail.com>
2018-09-13 02:47:13 -0300

    first view

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,9 +8,28 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
+
+# guess whether this is a runserver invocation with staticfiles
+has_staticfiles = False
+if 'runserver' in sys.argv:
+    from django.conf import settings
+    import inspect
+    if settings.DEBUG:
+        has_staticfiles = any(
+            "django/contrib/staticfiles/management/commands/runserver"
+            in x[1]
+            for x in inspect.stack())
+
+if has_staticfiles:
+    print('runserver with staticfiles detected, skipping whitenoise')
+else:
+    # Do not use whitenoise with runserver, as it's slow
+    from whitenoise.django import DjangoWhiteNoise
+    application = DjangoWhiteNoise(application)
